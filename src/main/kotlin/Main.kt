1a2baca7fae573169c6474f3e014313ba1b90bb9
Artem Oganesyan <krutoypan3@gmail.com>
2024-12-11 14:04:36 +0300

    Запуск через jar

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,7 +3,14 @@ import vk.VkServer
 import java.io.File
 
 fun main() {
-    val file = File("config.txt")
+    val file = File("config.json")
+
+    if (!file.exists()) {
+        println("ERROR: config file not found: ${file.absolutePath}")
+        Thread.sleep(3000)
+        return
+    }
+
     val content = file.readText()
     println("Load config from file: ${file.absolutePath}")
     val botConfig = Gson().fromJson(content, BotConfig::class.java)
